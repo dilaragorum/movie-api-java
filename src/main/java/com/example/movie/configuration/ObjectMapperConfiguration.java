a032abef936e7f11e45d25bd03b7c856d107afff
dilaragorum <dilaragorum@gmail.com>
2023-10-11 23:54:46 +0300

    implements Kafka Producer

--- src/main/java/com/example/movie/configuration/ObjectMapperConfiguration.java
+++ src/main/java/com/example/movie/configuration/ObjectMapperConfiguration.java
@@ -3,6 +3,7 @@ package com.example.movie.configuration;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +12,7 @@ public class ObjectMapperConfiguration {
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
         return objectMapper;
     }
 }
